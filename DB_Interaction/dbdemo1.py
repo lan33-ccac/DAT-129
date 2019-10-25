2308fd5fd94694f29b295fe3fc2a02b897745246
Lisa Nydick <lisa.nydick@gmail.com>
2019-10-25 13:56:54 -0400

    Initial Commit

---
+++ DB_Interaction/dbdemo1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#####################################
+# DAT-129
+# Purpose: Sample code for basic database operations using an SQLite database
+# Author: Lisa Nydick
+# Last Modified: 10/25/2019
+#####################################
+import sqlite3
+
+try:
+    #Make the connection object
+    dbconn = sqlite3.connect('SQLite_Python_Demo1.db')
+    
+    #Make the cursor object
+    cursor = dbconn.cursor()
+    print("Database created and Successfully Connected to SQLite")
+
+    #Build a query to select the version number from SQLie
+    select_Query = "select sqlite_version();"
+    
+    #Execute the query with the cursor object
+    cursor.execute(select_Query)
+    
+    #Fetch the first record (there will only be one)
+    record = cursor.fetchone()      #use fetchall or fetchmany to retrieve multiple records
+    print("SQLite Database Version is: ", record)
+    
+ 
+except sqlite3.Error as error:
+    print("Error while connecting to SQlite", error)
+finally:
+    #Always close the cursor and database connection
+    if dbconn:
+        cursor.close()
+        dbconn.close()
+        print("The SQLite connection is closed.")
+
---
+++ DB_Interaction/dbdemo2.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+#####################################
+# DAT-129
+# Purpose: Sample code for manipulating an SQLite Database
+# Author: Lisa Nydick
+# Last Modified: 10/25/2019
+#####################################
+
+import sqlite3
+import pandas as pd
+
+###################################################################################################
+# 
+###################################################################################################
+def main():
+    
+    
+    #Create the db connection and cursor
+    dbconn, cursor = connect_SQLiteDB()
+    
+    # If dbconn is something other than None, the database operation completed without error.
+    # Don't do subsequent operations if there was a failure in the previous routine.
+    if dbconn:
+        #Create a table in the database
+        dbconn = create_Table(dbconn, cursor)
+        if dbconn:
+            #Insert a couple of rows into the new table
+            dbconn = insert_Rows(dbconn, cursor)            
+            if dbconn:
+                #Update a value in a row
+                dbconn = update_Row(dbconn, cursor)
+                if dbconn:
+                    #Select and display all rows in the table
+                    dbconn = select_Rows(dbconn, cursor)
+                    if dbconn:
+                        #Delete all rows in the database
+                        dbconn = delete_Rows(dbconn, cursor)
+                        if dbconn:
+                            #Delete the new table
+                            dbconn = drop_Table(dbconn, cursor)
+                            if dbconn:
+                                #Close the DB connection and cursor
+                                close_DB_Resources(dbconn, cursor)
+                        
+###################################################################################################
+# Creates and returns a database connection object and a cursor object
+# If the DB doesn't already exist, it will be created.
+###################################################################################################    
+def connect_SQLiteDB():
+    try:
+        dbconn = sqlite3.connect('SQLite_Python_Demo2.db')
+        cursor = dbconn.cursor()
+        print()
+        print("Successfully Connected to SQLite Database")
+               
+    except sqlite3.Error as error:
+        print("Error while connecting to SQLite", error)
+        dbconn = handle_DB_Error(dbconn, cursor)
+        
+    return dbconn, cursor
+
+###################################################################################################
+# Creates a table called "users" in the database
+###################################################################################################       
+def create_Table(dbconn, cursor):
+
+    query_create_table ='''CREATE TABLE IF NOT EXISTS
+                            users(id INTEGER PRIMARY KEY, name TEXT,
+                            phone TEXT, email TEXT unique, password TEXT)'''
+
+# Type Conversions:
+# Python Type   SQLite Type
+# -----------   -----------
+# str           TEXT
+# int           INTEGER
+# float         REAL
+# byte          BLOB
+# None          Null
+
+    try:
+        #Execute the query
+        cursor.execute(query_create_table)
+        #Commit the transaction
+        dbconn.commit()
+        print()
+        print('Table created or accessed successfully.')
+    
+    except sqlite3.Error as error: 
+        print('Error occurred when creating table.', error)
+        dbconn = handle_DB_Error(dbconn, cursor)        
+    
+    #This will be None if there was an error
+    return dbconn    
+    
+###################################################################################################
+# Inserts 2 rows into the new table
+###################################################################################################
+def insert_Rows(dbconn, cursor):
+    name1 = 'Lisa'
+    phone1 = '4124018564'
+    email1 = 'lisa@nydick.com'
+    pwd1 = 'baseba11'
+    
+    name2 = 'Dan'
+    phone2 = '4129013268'
+    email2 = 'dan@nydick,com'
+    pwd2 = 'xfds4@83'    
+    
+    #Build a list of tuples
+    users = [(name1, phone1, email1, pwd1),
+             (name2, phone2, email2, pwd2)]
+    
+    #Parameterized query
+    insert_query = '''INSERT INTO users(name, phone, email, password)
+                  VALUES(?,?,?,?)'''
+
+    try: 
+        #Use executemany method to insert the list of tuples                 
+        cursor.executemany(insert_query, users)
+        dbconn.commit()
+        print()
+        print('Rows added successfully.')
+
+    except sqlite3.IntegrityError:
+        print('Record(s) already exist(s).')
+        dbconn.rollback()
+
+    except sqlite3.Error as error: 
+        print('Error occurred when adding a user.', error)
+        dbconn = handle_DB_Error(dbconn, cursor)
+
+    return dbconn
+
+###################################################################################################
+# Updates a field value in the table
+###################################################################################################            
+def update_Row(dbconn, cursor):
+    
+    old_email = 'lisa@nydick.com'
+    new_email = 'lisa.nydick@gmail.com'
+    update_query = '''UPDATE users SET email = ? WHERE email = ? '''
+     
+    try:
+        cursor.execute(update_query, (new_email, old_email))
+        dbconn.commit()
+        print()
+        print('Value updated successfully.')
+        
+    except sqlite3.Error as error: 
+        print('Error occurred when updating a user.', error)
+        dbconn = handle_DB_Error(dbconn, cursor)
+    
+    return dbconn
+
+###################################################################################################
+# Selects all rows from the table and displays them with a print statement
+# Also loads a pandas dataframe from sql and displays the contents of the dataframe
+###################################################################################################
+def select_Rows(dbconn, cursor):
+    
+    select_query = '''Select * from users'''
+    
+    try:
+        cursor.execute(select_query)
+        rows = cursor.fetchall()
+        print()
+        for row in rows:
+            print('{0} : {1}, {2}, {3}'.format(row[0], row[1], row[2], row[3]))
+            
+        #To store recordset in a Pandas dataframe:
+        df= pd.io.sql.read_sql(select_query, dbconn)
+        print()
+        print('Pandas dataframe containing recordset:')
+        pd.set_option('display.max_columns', 5)
+        pd.set_option('display.width', 800)
+        df.set_index('id', inplace = True)
+        print(df)
+        
+        
+    except sqlite3.Error as error: 
+        print('Database error occurred when selecting records.', error)
+        dbconn = handle_DB_Error(dbconn, cursor)
+    except Exception as err:    #in case a Pandas error occurs
+        print('Unexpected error occurred when displaying records.', err)
+        
+    return dbconn
+
+###################################################################################################
+# Deletes all rows from the users table
+###################################################################################################
+def delete_Rows(dbconn, cursor):
+    
+    delete_query = '''DELETE FROM users'''
+    
+    try:
+        cursor.execute(delete_query)
+        dbconn.commit()
+        print()
+        print('Deleted all users successfully.')
+
+    except sqlite3.Error as error: 
+        print('Error occurred when deleting user records.', error)
+        dbconn = handle_DB_Error(dbconn, cursor)
+        
+    return dbconn
+
+###################################################################################################
+# Deletes the users table
+###################################################################################################            
+def drop_Table(dbconn, cursor):
+    
+    drop_query = '''DROP TABLE users'''
+    
+    try:
+        cursor.execute(drop_query)
+        dbconn.commit()
+        print()
+        print('Dropped table successfully.')
+        
+    except sqlite3.Error as error: 
+        print('Error occurred when dropping table.', error)
+        dbconn = handle_DB_Error(dbconn, cursor)        
+
+    return dbconn
+
+###################################################################################################
+# Handles sqlite3 errors by rolling back transactions and closing DB resources
+###################################################################################################
+def handle_DB_Error(dbconn, cursor):
+    if dbconn:
+        try:
+            #rollback changes since the last commit
+            dbconn.rollback()
+            print('Rolled back transaction.')
+
+        except sqlite3.Error as error:
+            print('Error rolling back transaction.', error)
+            
+        finally:
+            #call function to close DB connection and cursor
+            close_DB_Resources(dbconn, cursor)        
+            return dbconn
+
+###################################################################################################
+# Closes the DB Connection and Cursor so the DB won't become locked
+###################################################################################################    
+def close_DB_Resources(dbconn, cursor):
+    try:
+        cursor.close()
+        dbconn.close()
+        print()
+        print('DB resources were closed successfully.')
+    except sqlite3.Error as error:
+        print('Error occurred closing DB resources.', error)
+        
+###################################################################################################
+if __name__=='__main__':
+    main()
+
+
+
+
